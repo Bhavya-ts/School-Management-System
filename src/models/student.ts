b18a35374aa4997a0a0756ab220c46e118f2e3a8
krupali <krupali.yadav@talentsystems.com>
2024-08-02 11:58:05 +0530

    schema added

--- src/models/student.ts
+++ src/models/student.ts
@@ -1,9 +1,28 @@
 import mongoose from "mongoose";
 
-const Schema = mongoose.Schema;
-
-
-const studentSchema = new Schema({
- 
-
+const bodyMeasurement = new mongoose.Schema({
+  month: String,
+  year: Number,
+  height: Number,
+  weight: Number,
 });
+
+const StudentSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  rollNo: {
+    type: String,
+  },
+  age: {
+    type: Number,
+  },
+  std_details: [],
+  std: {
+    type: Number,
+    required: true,
+  },
+  body_measurement: [bodyMeasurement],
+});
+export const studentModel = mongoose.model("student", StudentSchema);
